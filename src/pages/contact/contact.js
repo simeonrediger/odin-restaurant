4cc0a33524963b40052a7f6c9ed50b6bbd1ad17c
Simeon Rediger <simeonrediger@gmail.com>
2025-09-21 11:35:22 -0400

    Style contact page

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,18 +1,24 @@
+import './contact.css';
+
 export default function render(container) {
     const form = document.createElement('form');
 
     const formTitle = document.createElement('h2');
     formTitle.textContent = 'Beseech your Lord...';
+    formTitle.classList.add('form-title');
 
     const emailInput = document.createElement('input');
     emailInput.type = 'email';
     emailInput.placeholder = 'Thine email';
+    emailInput.classList.add('email-input', 'input');
 
     const messageInput = document.createElement('textarea');
     messageInput.placeholder = 'Thy bidding'
+    messageInput.classList.add('message-input', 'input');
 
     const submitButton = document.createElement('button');
     submitButton.textContent = 'Submit to your Lord';
+    submitButton.classList.add('submit-button', 'button');
 
     form.append(
         formTitle,
