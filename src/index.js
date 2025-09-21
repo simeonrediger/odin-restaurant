b6c4992a75d128d8657f950856d1c00b9a330712
Simeon Rediger <simeonrediger@gmail.com>
2025-09-20 20:14:21 -0400

    Style button defaults

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/base.css';
 import './styles/layout.css';
+import './styles/button.css';
 
 import container from './app/container.js';
 import renderHomePage from './pages/home-page.js';
