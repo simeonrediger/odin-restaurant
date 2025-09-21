d127d23518e067e68dc39dccb0cd37566bf45f81
Simeon Rediger <simeonrediger@gmail.com>
2025-09-21 09:38:22 -0400

    Add menu content

--- src/pages/menu/menu.js
+++ src/pages/menu/menu.js
@@ -1,15 +1,28 @@
+import dishImage from '../../assets/images/bread-grapes-and-wine.jpg';
+
 export default function render(container) {
-    const testElement = document.createElement('p');
-    testElement.textContent = `
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero amet
-        possimus porro deleniti enim magni, sint ab ut aspernatur sunt quia ex
-        at voluptas, quaerat vel. Odit suscipit nulla dolorem.
+    const dishImageElement = document.createElement('img');
+    dishImageElement.src = dishImage;
+    dishImageElement.alt = 'Bread, grapes, and wine';
+    dishImageElement.title = 'Photo by Marek Studzinski on Unsplash';
+    dishImageElement.width = '240';
+
+    const dishName = document.createElement('h2');
+    dishName.textContent = 'The Ninth Life Supper';
+
+    const dishDescription = document.createElement('p');
+    dishDescription.textContent = `
+        An opulent final indulgence decreed by our feline sovereign. Rich, dark
+        flavors layered with subtle sweetness, crafted to linger like a
+        whispered secret you were never meant to hear.
     `;
 
     const content = document.createElement('div');
     content.id = 'menu';
     content.append(
-        testElement,
+        dishImageElement,
+        dishName,
+        dishDescription,
     );
 
     container.append(content);
