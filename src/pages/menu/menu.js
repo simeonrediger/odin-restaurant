6cef7f3927947c19a98e7c89123e65fdbe01cf80
Simeon Rediger <simeonrediger@gmail.com>
2025-09-21 10:28:24 -0400

    Style menu page

--- src/pages/menu/menu.js
+++ src/pages/menu/menu.js
@@ -1,14 +1,18 @@
+import './menu.css';
 import dishImage from '../../assets/images/bread-grapes-and-wine.jpg';
+import currencySymbol from '../../assets/icons/skull.svg';
 
 export default function render(container) {
     const dishImageElement = document.createElement('img');
     dishImageElement.src = dishImage;
     dishImageElement.alt = 'Bread, grapes, and wine';
     dishImageElement.title = 'Photo by Marek Studzinski on Unsplash';
-    dishImageElement.width = '240';
+    dishImageElement.width = '272';
+    dishImageElement.classList.add('dish-image');
 
     const dishName = document.createElement('h2');
     dishName.textContent = 'The Ninth Life Supper';
+    dishName.classList.add('dish-name');
 
     const dishDescription = document.createElement('p');
     dishDescription.textContent = `
@@ -16,6 +20,14 @@ export default function render(container) {
         flavors layered with subtle sweetness, crafted to linger like a
         whispered secret you were never meant to hear.
     `;
+    dishDescription.classList.add('dish-description');
+
+    const dishPrice = document.createElement('div');
+    const currencySymbolIcon = document.createElement('img');
+    currencySymbolIcon.src = currencySymbol;
+    const priceText = document.createTextNode('1 soul');
+    dishPrice.append(currencySymbolIcon, priceText);
+    dishPrice.classList.add('dish-price');
 
     const content = document.createElement('div');
     content.id = 'menu';
@@ -23,6 +35,7 @@ export default function render(container) {
         dishImageElement,
         dishName,
         dishDescription,
+        dishPrice,
     );
 
     container.append(content);
