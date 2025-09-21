a7745625f9b2617b31519b8a81bc9da2ced52fe9
Simeon Rediger <simeonrediger@gmail.com>
2025-09-21 12:25:21 -0400

    Add fade transition between page loads

--- src/app/container.js
+++ src/app/container.js
@@ -1,9 +1,19 @@
+import { delay } from './utilities.js';
+
 export default (function () {
     const element = document.getElementById('content');
 
-    function render(renderPage) {
+    async function render(renderPage) {
+        hide();
+        await delay(200);
         empty();
         renderPage(element);
+        await delay(200);
+        show();
+    }
+
+    function hide() {
+        element.classList.add('hidden');
     }
 
     function empty() {
@@ -11,6 +21,10 @@ export default (function () {
         while (element.firstChild) {
             element.firstChild.remove();
         }
+    }
+
+    function show() {
+        element.classList.remove('hidden');
     }
 
     return {
